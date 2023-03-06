e1b861e78f1b300cada05a7607e8211582a99273
Abner Silva <contato@altechno.com.br>
2023-03-05 22:53:17 -0300

    ajustes de estilo de tags layout

--- frontend/src/pages/details/DetailsPage.jsx
+++ frontend/src/pages/details/DetailsPage.jsx
@@ -6,53 +6,52 @@ import './Index.css'
 function DetailsPage() {
     return (
         <div className='row'>
-            <div>
-                <SearchComponent />
-            </div>
+            <SearchComponent />
 
             <hr />
-
+            <br />
             <div className='container'>
-                <h3 >Processo n. 1008126-92.2015.8.26.0132 do TJSP</h3>
+                <div className='container'>
+                    <h3 >Processo n. 1008126-92.2015.8.26.0132 do TJSP</h3>
+                    <p className='detalhe'>Distribuído em 09/11/2017</p>
 
-                <p className=''>Distribuído em 09/11/2017</p>
-
-                <div className='row'>
-                    <div className='col-8'>
-                        <div role="tabpanel">
-                            <div className="list-group">
-                                {/* <p className="list-group-item list-group-item-secondary" role="tab">Movimentações</p> */}
-                                <div className="list-group-item" role="tab">
-                                    <p className='detalhe'>11/06/2018</p>
-                                    <p style={{ fontSize: 'small', textAlign: 'start' }}>Profile</p>
-                                </div>
-                                <div className="list-group-item" role="tab">
-                                    <p className='detalhe'>11/06/2018</p>
-                                    <p style={{ fontSize: 'small', textAlign: 'start' }}>Profile</p>
-                                </div>
-                                <div className="list-group-item" role="tab">
-                                    <p className='detalhe'>11/06/2018</p>
-                                    <p style={{ fontSize: 'small', textAlign: 'start' }}>Profile</p>
-                                </div>
-                                <div className="list-group-item" role="tab">
-                                    <p className='detalhe'>11/06/2018</p>
-                                    <p style={{ fontSize: 'small', textAlign: 'start' }}>Profile</p>
+                    <div className='row'>
+                        <div className='col-8'>
+                            <div role="tabpanel">
+                                <div className="list-group">
+                                    <h4 className='list-group-item list-group-item-secondary'>Movimentações</h4>
+                                    <div className="list-group-item" role="tab">
+                                        <p className='detalhe'>11/06/2018</p>
+                                        <p style={{ fontSize: 'small', textAlign: 'start' }}>Descrição da movimentação do processo</p>
+                                    </div>
+                                    <div className="list-group-item" role="tab">
+                                        <p className='detalhe'>11/06/2018</p>
+                                        <p style={{ fontSize: 'small', textAlign: 'start' }}>Descrição da movimentação do processo</p>
+                                    </div>
+                                    <div className="list-group-item" role="tab">
+                                        <p className='detalhe'>11/06/2018</p>
+                                        <p style={{ fontSize: 'small', textAlign: 'start' }}>Descrição da movimentação do processo</p>
+                                    </div>
+                                    <div className="list-group-item" role="tab">
+                                        <p className='detalhe'>11/06/2018</p>
+                                        <p style={{ fontSize: 'small', textAlign: 'start' }}>Descrição da movimentação do processo</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="col-4">
-                        <p>Detalhes do processo</p>
-                        <p className='detalhe'>Cumprimento de sentença - Honorários advocatícios</p>
-                        <p className='detalhe'>TJSP - Forum João Mendes Júnior Balneários Camburiú - 4º Vara Civil</p>
-                        <p className='detalhe'>Processo Judicial - Rito ordinário</p>
-                        <p className='detalhe'>Processo julgado - 1ª Instância - Valor de caus: R$ 1.387,90</p>
+                        <div className="col-4">
+                            <p>Detalhes do processo</p>
+                            <p className='detalhe'>Cumprimento de sentença - Honorários advocatícios</p>
+                            <p className='detalhe'>TJSP - Forum João Mendes Júnior Balneários Camburiú - 4º Vara Civil</p>
+                            <p className='detalhe'>Processo Judicial - Rito ordinário</p>
+                            <p className='detalhe'>Processo julgado - 1ª Instância - Valor de caus: R$ 1.387,90</p>
 
-                        <hr />
+                            <hr />
 
-                        <p>Partes envolvidas</p>
-                        <p className='detalhe'>Nelson Willian & Advogados Associados</p>
+                            <p>Partes envolvidas</p>
+                            <p className='detalhe'>Nelson Willian & Advogados Associados</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import SearchComponent from '../../components/SearchComponent';
 
+import './Index.css'
+
 function HomePage() {
-    return(
+    return (
         <>
-            <div className='container-fluid' style={{backgroundColor: '#c7c7c7', padding: '30px'}}>
-                <h3 style={{textAlign:'left'}}>Buscar</h3>
+            <div className='container-fluid' style={{ backgroundColor: '#c7c7c7', padding: '30px' }}>
+                <h3 style={{ textAlign: 'left' }}>Buscar</h3>
 
                 Selecione um tribunal para lsitar os processos ou buscar pelo número unificado.
                 <br />
                 <br />
-                
+
                 <SearchComponent />
             </div>
         </>
