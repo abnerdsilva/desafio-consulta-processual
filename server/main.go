b1c434c192ae873f7d2a3613616dee646030839a
Abner Silva <contato@altechno.com.br>
2023-03-07 23:15:43 -0300

    add server static para projeto web

--- server/main.go
+++ server/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"github.com/abnerdsilva/desafio-consulta-processual/controller"
 	"github.com/abnerdsilva/desafio-consulta-processual/model/repository"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	dbFile := "../../../DB/processos.json"
+	dbFile := "../DB/processos.json"
 
 	processRepo := repository.NewProcessoRepository(dbFile)
 	processControll := controller.NewProcessoController(processRepo)
 
 	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("../build", false)))
+
 	router.GET("/process", processControll.GetProcessos)
 	router.GET("/process/:id", processControll.GetProcesso)
 	router.POST("/process", processControll.CreateProcesso)
